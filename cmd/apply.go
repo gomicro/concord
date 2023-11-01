8153a655fbbfda182d04c46ed664f4dc079c3d52
Daniel Hess <dan9186@gmail.com>
2023-11-01 15:27:13 -0700

    break out apply command

--- cmd/apply.go
+++ cmd/apply.go
@@ -1,22 +1,26 @@
 package cmd
 
 import (
+	"context"
 	"io"
 	"os"
 
+	"github.com/gomicro/concord/report"
 	"github.com/spf13/cobra"
 )
 
+var applyCmd = NewApplyCmd(os.Stdout)
+
 func init() {
-	rootCmd.AddCommand(NewApplyCmd(os.Stdout))
+	rootCmd.AddCommand(applyCmd)
 }
 
 func NewApplyCmd(out io.Writer) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:               "apply",
 		Args:              cobra.ExactArgs(1),
-		Short:             "Apply a github configuration",
-		Long:              `Apply a github configuration`,
+		Short:             "Apply an org configuration",
+		Long:              `Apply an org configuration against github`,
 		PersistentPreRunE: setupClient,
 		RunE:              applyRun,
 	}
@@ -27,5 +31,31 @@ func NewApplyCmd(out io.Writer) *cobra.Command {
 }
 
 func applyRun(cmd *cobra.Command, args []string) error {
+	ctx := context.Background()
+	file := args[0]
+
+	org, err := readManifest(file)
+	if err != nil {
+		return handleError(cmd, err)
+	}
+
+	report.PrintHeader("Org")
+	report.Println()
+
+	err = membersRun(ctx, cmd, args, org, false)
+	if err != nil {
+		return handleError(cmd, err)
+	}
+
+	err = teamsRun(ctx, cmd, args, org, false)
+	if err != nil {
+		return handleError(cmd, err)
+	}
+
+	err = reposRun(ctx, cmd, args, org, false)
+	if err != nil {
+		return handleError(cmd, err)
+	}
+
 	return nil
 }
---
+++ cmd/apply_members.go
@@ -0,0 +1,44 @@
+package cmd
+
+import (
+	"context"
+	"io"
+	"os"
+
+	"github.com/gomicro/concord/report"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	applyCmd.AddCommand(NewApplyMembersCmd(os.Stdout))
+}
+
+func NewApplyMembersCmd(out io.Writer) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:               "members",
+		Args:              cobra.ExactArgs(1),
+		Short:             "Apply a members configuration",
+		Long:              `Apply members in a configuration against github`,
+		PersistentPreRunE: setupClient,
+		RunE:              applyMembersRun,
+	}
+
+	cmd.SetOut(out)
+
+	return cmd
+}
+
+func applyMembersRun(cmd *cobra.Command, args []string) error {
+	ctx := context.Background()
+	file := args[0]
+
+	org, err := readManifest(file)
+	if err != nil {
+		return handleError(cmd, err)
+	}
+
+	report.PrintHeader("Org")
+	report.Println()
+
+	return membersRun(ctx, cmd, args, org, false)
+}
